08957369fa8201dcb5257a66d7856a4e7cfd41e6
Dipesh Acharya <xtranophilist@gmail.com>
2014-10-15 18:45:16 +0545

    replaced AppSetting model with dbsetting

--- core/models.py
+++ core/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 
-from app.libr import MultiNameModel, transl, ne2en
+from app.libr import MultiNameModel, transl
+import dbsettings
 
+FISCAL_YEARS = (
+    (2069, "2069/70"),
+    (2070, "2070/71"),
+    (2071, "2071/72")
+)
 
 SOURCES = [('nepal_government', 'Nepal Government'), ('foreign_cash_grant', 'Foreign Cash Grant'),
            ('foreign_compensating_grant', 'Foreign Compensating Grant'), ('foreign_cash_loan', 'Foreign Cash Loan'),
            ('foreign_compensating_loan', 'Foreign Compensating Loan'),
            ('foreign_substantial_aid', 'Foreign Substantial Aid')]
+
+
+class AppSetting(dbsettings.Group):
+    site_name = dbsettings.StringValue(default='NERP')
+    fiscal_year = dbsettings.StringValue(choices=FISCAL_YEARS)
+    header_for_forms = dbsettings.TextValue()
+
+
+app_setting = AppSetting()
 
 
 class Language(models.Model):
@@ -40,24 +55,10 @@ class Party(MultiNameModel):
 
 
 class FiscalYear(models.Model):
-    years = (
-        (2069, "2069/70"),
-        (2070, "2070/71"),
-        (2071, "2071/72")
-    )
-    year = models.IntegerField(choices=years, unique=True)
+    year = models.IntegerField(choices=FISCAL_YEARS, unique=True)
 
     def __str__(self):
         return str(self.year) + '/' + str(self.year - 1999)
-
-
-class AppSetting(models.Model):
-    app_name = models.CharField(max_length=254)
-    fiscal_year = models.ForeignKey(FiscalYear)
-    header_for_forms = models.TextField()
-
-    def __str__(self):
-        return 'Default Settings ' + str(self.id)
 
 
 class Employee(MultiNameModel):
