c52ebfd04f504f16d5aa3f4549de02d2e5fed2da
Dipesh Acharya <xtranophilist@gmail.com>
2014-12-16 20:55:21 +0545

    fixed admin for core models

--- core/admin.py
+++ core/admin.py
@@ -5,12 +5,12 @@ from core.models import FiscalYear, Donor, Activity, BudgetHead, Employee, Party
     Language
 
 admin.site.register(Party, TranslationAdmin)
-admin.site.register(FiscalYear, TranslationAdmin)
+admin.site.register(FiscalYear)
 admin.site.register(Donor, TranslationAdmin)
 admin.site.register(Activity, TranslationAdmin)
 admin.site.register(BudgetHead, TranslationAdmin)
-admin.site.register(BudgetBalance, TranslationAdmin)
+admin.site.register(BudgetBalance)
 admin.site.register(Employee, TranslationAdmin)
 admin.site.register(Account, TranslationAdmin)
 admin.site.register(TaxScheme, TranslationAdmin)
-admin.site.register(Language, TranslationAdmin)
+admin.site.register(Language)
