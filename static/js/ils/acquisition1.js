c84fe5e465d9dc845a253084851446880fef6717
wrufesh <wrufesh@gmail.com>
2015-02-01 18:07:04 +0545

    Everything working good

--- static/js/ils/acquisition1.js
+++ static/js/ils/acquisition1.js
@@ -11,4 +11,34 @@ AcquisionVM = function(){
 	self.publishers = ko.observableArray();
 	self.languages = ko.observableArray();
 
+	self.ajaxPost = function(url, optionField, oble, selectize_element, formElement){
+			var x = $('formElement, input').serialize();
+			$.post(url,x
+				).success(function(data){
+					console.log(data);
+					createdObj = {
+						text: data[optionField],
+						value: data.id,
+					};
+					self[oble].push(createdObj);
+
+					var $select = $(selectize_element).selectize({
+					});
+					var selectize = $select[0].selectize;
+					selectize.addOption(createdObj);
+					selectize.addItem(data.id);
+
+					var hh = $(formElement).find('.close-reveal-modal');
+					$(hh).trigger("click");
+
+				}).done(function(){
+					var errorHtml = '<div data-alert class="alert-box success"><label> Option successfully ADDED and SELECTED!</label> <a href="#" class="close">&times;</a></div>';
+					$('#main-error-area').append(errorHtml).foundation();
+					// $('#main-error-area').fadeOut(6000, "swing");
+				}).error(function(data){
+					var errorHtml = '<div data-alert class="alert-box round">'+ data.responseText +'<a href="#" class="close">&times;</a></div>';
+					$(formElement).append(errorHtml);
+				});
+		};
+
 };
