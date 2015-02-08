df31f8352eb3f4262f0df4b9479262b2ce53e5b8
wrufesh <wrufesh@gmail.com>
2015-02-08 15:17:06 +0545

    number input fixed

--- static/js/ils/acquisition1.js
+++ static/js/ils/acquisition1.js
@@ -18,55 +18,6 @@ $(document).ready(function () {
             };
         }
     });
-
-    // var selectize = $select[0].selectize;
-    // selectize.on('item_add', function(value, $item){
-    //     console.log(value);
-    //     // console.log(data);
-
-    // });
     $('#id_languages').selectize({
     });
-//     acq = new AcquisionVM();
-// 	    ko.applyBindings(acq);
-// 	    });
-// AcquisionVM = function(){
-// 	self = this;
-// 	self.books = ko.observableArray();
-// 	self.subjects = ko.observableArray();
-// 	self.authors = ko.observableArray();
-// 	self.pub_places = ko.observableArray();
-// 	self.publishers = ko.observableArray();
-// 	self.languages = ko.observableArray();
-
-// 	self.ajaxPost = function(url, optionField, oble, selectize_element, formElement){
-// 			var x = $('input, select').serialize();
-// 			$.post(url,x
-// 				).success(function(data){
-// 					console.log(data);
-// 					createdObj = {
-// 						text: data[optionField],
-// 						value: data.id,
-// 					};
-// 					self[oble].push(createdObj);
-
-// 					var $select = $(selectize_element).selectize({
-// 					});
-// 					var selectize = $select[0].selectize;
-// 					selectize.addOption(createdObj);
-// 					selectize.addItem(data.id);
-
-// 					// var hh = $(formElement).find('.close-reveal-modal');
-// 					// $(hh).trigger("click");
-
-// 				}).done(function(){
-// 					var errorHtml = '<div data-alert class="alert-box success"><label> Option successfully ADDED and SELECTED!</label> <a href="#" class="close">&times;</a></div>';
-// 					$('#main-error-area').append(errorHtml).foundation();
-// 					// $('#main-error-area').fadeOut(6000, "swing");
-// 				}).error(function(data){
-// 					var errorHtml = '<div data-alert class="alert-box round">'+ data.responseText +'<a href="#" class="close">&times;</a></div>';
-// 					$(formElement).append(errorHtml);
-// 				});
-// 		};
-
 });
