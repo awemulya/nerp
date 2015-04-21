aa1764a0dcae85760d0721d634e0ff48c813ed82
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-04-21 12:20:59 +0545

    inspection added to admin-site

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from models import Item, InventoryAccount, Demand
+from models import Item, InventoryAccount, Demand, Inspection, InspectionRow
 
 admin.site.register(Item)
 admin.site.register(InventoryAccount)
 admin.site.register(Demand)
+admin.site.register(Inspection)
+admin.site.register(InspectionRow)
