4dc3d990e5458f6d89100ab7165fd9aef8b34950
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-06-04 14:24:45 +0545

    mf to item.js

--- static/js/inventory/item.js
+++ static/js/inventory/item.js
@@ -1,8 +1,6 @@
 $(document).ready(function () {
     item = new ItemVM(item_data);
-    // ko.applyBindings(item);
     var item_form = document.getElementById("other-properties");
-    // ko.cleanNode(item_form);
     ko.applyBindings(item, item_form);
     $('.change-on-ready').trigger('change');
 });
@@ -13,13 +11,11 @@ function ItemVM(data) {
 	self.other_properties = ko.observableArray([]);
     
     if (data != null) {
-    	// var test = JSON.parse(data);
     	for (item_property in data) {
     		var property_name = item_property
     		var property = data[item_property]
     		self.other_properties.push(new OtherPropertiesVM().property_name(property_name).property(property))
     	}
-    	// debugger;
     } else {
     	self.other_properties.push(new OtherPropertiesVM())
     }
