5fc100f8206722744c64ca5c2e648cc7384477f1
Dipesh Acharya <xtranophilist@gmail.com>
2015-07-10 16:26:23 +0545

    removed release_quantity from DemandRow in favor of Release model

--- inventory/models.py
+++ inventory/models.py
@@ -362,7 +362,7 @@ class DemandRow(models.Model):
     specification = models.CharField(max_length=254, blank=True, null=True)
     quantity = models.FloatField()
     unit = models.CharField(max_length=50)
-    release_quantity = models.FloatField(null=True, blank=True)
+    # release_quantity = models.FloatField(null=True, blank=True)
     remarks = models.CharField(max_length=254, blank=True, null=True)
     demand = models.ForeignKey(Demand, related_name='rows')
     statuses = [('Requested', 'Requested'), ('Approved', 'Approved'), ('Fulfilled', 'Fulfilled')]
