bc9f9ff18eb2548d97d9b0219a73136b0318c106
Dipesh Acharya <xtranophilist@gmail.com>
2015-07-21 18:27:17 +0545

    improved admin for core models

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,25 @@ from app.utils.translation import TranslationAdmin
 from core.models import FiscalYear, Donor, Activity, BudgetHead, Employee, Party, Account, TaxScheme, BudgetBalance, \
     Language
 
-admin.site.register(Party, TranslationAdmin)
+
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'name', 'code')
+    list_display_links = ('__unicode__', 'name', 'code')
+    search_fields = ('name', 'code')
+
+
+class PartyAdmin(TranslationAdmin):
+    list_display = ('__unicode__', 'address', 'phone_no', 'pan_no')
+    list_display_links = ('__unicode__', 'pan_no', 'phone_no')
+    search_fields = ('address', 'phone_no', 'pan_no')
+
+
+class TaxSchemeAdmin(TranslationAdmin):
+    list_display = ('name', 'percent')
+    search_fields = ('name', 'percent')
+
+
+admin.site.register(Party, PartyAdmin)
 admin.site.register(FiscalYear)
 admin.site.register(Donor, TranslationAdmin)
 admin.site.register(Activity, TranslationAdmin)
@@ -11,5 +29,5 @@ admin.site.register(BudgetHead, TranslationAdmin)
 admin.site.register(BudgetBalance)
 admin.site.register(Employee, TranslationAdmin)
 admin.site.register(Account, TranslationAdmin)
-admin.site.register(TaxScheme, TranslationAdmin)
-admin.site.register(Language)
+admin.site.register(TaxScheme, TaxSchemeAdmin)
+admin.site.register(Language, LanguageAdmin)
