159dc2ad475d09c421a819e078c853f563370959
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-08-17 12:00:52 +0545

    dynamic table load partially complete

--- static/js/inventory/quotation_comparison.js
+++ static/js/inventory/quotation_comparison.js
@@ -16,31 +16,6 @@ function QuotationComparisonVM(data){
 		}
 	});
 
-	$.ajax({
-		url: '/parties.json',
-		dataType: 'json',
-		async: false,
-		success: function (data) {
-			self.parties = ko.observableArray(data);
-		}
-	});
-	
-	self.selected_party = ko.observable()
-	self.party_display = ko.observableArray([])
-
-	self.add_party = function () {
-		for (o in self.parties() ){
-			if (self.parties()[o].id == self.selected_party()) {
-				self.party_display.push(self.parties()[o]);
-			}
-		}
-	}
-
-    for (i in data.rows) {
-    	self.party_display.push(data.rows[i].party.party)
-    }
-
-    self.table_view = new TableViewModel({rows: data.rows}, QuotationRow);
 
 	self.item_changed = function (row) {
 		var selected_item = $.grep(self.items(), function (i) {
@@ -55,30 +30,70 @@ function QuotationComparisonVM(data){
 		row.account_no(selected_item.account_no);
 	}
 
-    for (var k in data)
-        self[k] = ko.observable(data[k]);
+	$.ajax({
+		url: '/parties.json',
+		dataType: 'json',
+		async: false,
+		success: function (data) {
+			self.parties = ko.observableArray(data);
+		}
+	});
+
+	self.selected_party = ko.observable()
+
+	self.parties_to_display = ko.observableArray([])
 
 
+	self.add_party = function (row) {
+		for (o in self.parties()){
+			if (self.parties()[o].id == self.selected_party()) {
+				self.parties_to_display.push(self.parties()[o]);
+				for ( qr in row.rows()){
+					row.rows()[qr].partyVM.push(new PartyQuotationVM().bidder_name(self.parties()[o].name))
+				}
+			}
+		}
+		// self.parties.remove( function(item) {return item.id == self.selected_party() })
+  //       console.log(self.parties())
+	}
+
+    self.table_view = new TableViewModel({rows: data.rows, argument: self.parties_to_display() }, QuotationRow);
+
+
+  //   for (i in data.rows) {
+  //   	self.parties_to_display.push(data.rows[i].party.party)
+		// self.partyVM.push(new PartyQuotationVM().bidder_name(data.rows[i].party.party.name))
+  //   }
+	// console.log(self.parties())
 }
 
-function PartyQuotationVM(party) {
+function PartyQuotationVM() {
 	var self = this;
+	self.bidder_name = ko.observable();
 	self.per_unit_price = ko.observable();
-
 }
 
-function QuotationRow(row) {
+
+
+function QuotationRow(row, argument) {
 	var self = this;
+
 	self.item_id = ko.observable()
 	self.specification = ko.observable()
-	self.remarks = ko.observable()
-	self.quantity = ko.observable()
 	self.estimated_cost = ko.observable()
-    // debugger;
+	self.quantity = ko.observable()
     self.unit = ko.observable()
     self.inventory_classification_reference_no = ko.observable()
     self.account_no = ko.observable()
+	self.remarks = ko.observable()
 
+    self.partyVM = ko.observableArray()
+    if ( argument ){
+    	for (o in argument) {
+    		self.partyVM.push(new PartyQuotationVM().bidder_name(argument[o].name))
+    	}
+    }
+	
     for (var k in row) {
         if (row[k] != null)
             self[k] = ko.observable(row[k]);
