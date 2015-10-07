ad2067618772a2d35fe91b086e3b8e02af324e41
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-10-07 22:55:44 +0545

    mf

--- core/migrations/0002_auto_20151007_2145.py
+++ core/migrations/0002_auto_20151007_2145.py
@@ -4,6 +4,16 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+def fiscal_years(apps, schema_editor):
+	import ipdb; ipdb.set_trace()
+	print "test"
+	from core.models import FiscalYear
+
+	FiscalYear.objects.create(year=2070)
+	FiscalYear.objects.create(year=2071)
+
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,4 +21,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+    	migrations.RunPython(fiscal_years),
     ]
--- users/migrations/0002_auto_20151007_2107.py
+++ users/migrations/0002_auto_20151007_2107.py
@@ -7,19 +7,24 @@ from django.db import transaction
 
 
 def initialize(apps, schema_editor):
-    from users.models import User
-    try:
-        with transaction.atomic():
-            admin = User.objects.create_superuser('admin', 'webadmin@awecode.com', 'admin')
-    except IntegrityError:
-        admin = User.objects.get(username='admin')
+	from users.models import User
+	from django.contrib.auth.models import Group
+	import ipdb; ipdb.set_trace()
+
+	Group.objects.create(name='Store')
+
+	try:
+		with transaction.atomic():
+			admin = User.objects.create_superuser('admin', 'webadmin@awecode.com', 'admin')
+	except IntegrityError:
+		admin = User.objects.get(username='admin')
 
 
 class Migration(migrations.Migration):
-    dependencies = [
-        ('users', '0001_initial'),
-    ]
+	dependencies = [
+		('users', '0001_initial'),
+	]
 
-    operations = [
-    	migrations.RunPython(initialize),
-    ]
+	operations = [
+		migrations.RunPython(initialize),
+	]
