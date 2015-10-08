33714c75e40400552d4f16ccd1318e048616c1fb
Roshan Shrestha <roshanshrestha01@gmail.com>
2015-10-08 11:26:24 +0545

    data migrations for itemlocation, fiscal_year and usergroup

--- users/migrations/0002_auto_20151007_2107.py
+++ users/migrations/0002_auto_20151007_2107.py
@@ -6,25 +6,11 @@ from django.db import models, migrations
 from django.db import transaction
 
 
-def initialize(apps, schema_editor):
-	from users.models import User
-	from django.contrib.auth.models import Group
-	import ipdb; ipdb.set_trace()
-
-	Group.objects.create(name='Store')
-
-	try:
-		with transaction.atomic():
-			admin = User.objects.create_superuser('admin', 'webadmin@awecode.com', 'admin')
-	except IntegrityError:
-		admin = User.objects.get(username='admin')
-
-
 class Migration(migrations.Migration):
 	dependencies = [
 		('users', '0001_initial'),
 	]
 
 	operations = [
-		migrations.RunPython(initialize),
+		# migrations.RunPython(initialize),
 	]
