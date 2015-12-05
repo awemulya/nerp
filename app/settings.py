9a7d4987453403ef0248ef5ec68d0d61445a6529
Dipesh Acharya <xtranophilist@gmail.com>
2015-12-05 16:41:59 +0545

    mf

--- app/settings.py
+++ app/settings.py
@@ -96,7 +96,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'linaro_django_pagination.middleware.PaginationMiddleware',
     'webstack_django_sorting.middleware.SortingMiddleware',
     'njango.middleware.CalendarMiddleware',
