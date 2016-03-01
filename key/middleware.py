a1e617f3a25f67f87a030f3f254b9293a81e8ac9
Dipesh Acharya <xtranophilist@gmail.com>
2016-03-01 19:35:44 +0545

    Invalid key template

--- key/middleware.py
+++ key/middleware.py
@@ -1,7 +1,10 @@
 from dbsettings.models import Setting
-from django.http import HttpResponse
+
+# from django.http import HttpResponse
 # from django.shortcuts import redirect
 # from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
+
 from .coder import Coder
 
 from .config import *
@@ -27,16 +30,20 @@ def validate_key(key, user, app_name):
 
 class KeyMiddleware(object):
     def process_request(self, request):
-        setting = Setting.objects.get(attribute_name=ATTR_NAME)
-        if not setting.value in DEFAULT_VALUES and not request.path.replace('/', '')[0:5] == 'admin' and not request.path.replace(
-                '/', '')[0:3] == 'key':
-            validity = {'error': 'No Activation Key!'}
-            try:
-                key = Setting.objects.get(attribute_name='key')
-                validity = validate_key(key.value, setting.value, APP_NAME)
-            except Setting.DoesNotExist:
-                pass
-            if 'error' in validity:
-                # return redirect(reverse_lazy('invalid_key'))
-                return HttpResponse('<h2 style="text-align: center; margin-top: 20%">' + validity['error'] + '</h2>')
-        pass
+        try:
+            setting = Setting.objects.get(attribute_name=ATTR_NAME)
+            if not setting.value in DEFAULT_VALUES and not request.path.replace('/', '')[
+                                                           0:5] == 'admin' and not request.path.replace(
+                    '/', '')[0:3] == 'key':
+                validity = {'error': 'No Activation Key!'}
+                try:
+                    key = Setting.objects.get(attribute_name='key')
+                    validity = validate_key(key.value, setting.value, APP_NAME)
+                except Setting.DoesNotExist:
+                    pass
+                if 'error' in validity:
+                    # return HttpResponse('<h2 style="text-align: center; margin-top: 20%">' + validity['error'] + '</h2>')
+                    # return redirect(reverse_lazy('invalid_key'))
+                    return render(request, 'key/invalid.html', validity)
+        except Setting.DoesNotExist:
+            pass
--- key/urls.py
+++ key/urls.py
@@ -4,6 +4,6 @@ import views
 urlpatterns = patterns('',
 
                        url(r'^request/$', views.send_key_request, name='send_key_request'),
-                       url(r'^invalid/$', views.invalid_key, name='invalid_key'),
+                       # url(r'^invalid/$', views.invalid_key, name='invalid_key'),
 
                        )
