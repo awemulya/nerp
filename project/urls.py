e549848f46e9a70347d1075cc253ebd72fcf01b3
Dipesh Acharya <xtranophilist@gmail.com>
2016-05-02 21:09:02 +0545

    mtf

--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-                       url(r'^imprest_ledger$', views.ImprestLedger.as_view(), name='imprest_ledger'),
+                       url(r'^imprest_ledger/$', views.ImprestLedger.as_view(), name='imprest_ledger'),
                        # url(r'^receipt/(?P<pk>[0-9]+)/$', views.receipt, name='update_receipt'),
                        # url(r'^receipt/save/$', views.save_receipt, name='save_receipt'),
 
