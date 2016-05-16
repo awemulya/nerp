f4945df3720247e50df61d77b710d4e6af67cb19
Roshan Shrestha <roshanshrestha01@gmail.com>
2016-05-16 16:21:13 +0545

    Transaction and JournalEntry borrowed from awecounting

--- account/models.py
+++ account/models.py
@@ -1,5 +1,11 @@
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
+from app.utils.helpers import zero_for_none, none_for_zero
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from core.models import FiscalYear, Donor, Activity, Account, BudgetHead, TaxScheme
+import datetime
 
 
 class JournalVoucher(models.Model):
@@ -46,3 +52,139 @@ class ReceiptRow(models.Model):
     remarks = models.CharField(max_length=254, blank=True, null=True)
 
     receipt = models.ForeignKey(Receipt, related_name='rows')
+
+
+class JournalEntry(models.Model):
+    date = models.DateField()
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    source = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self):
+        return str(self.content_type) + ': ' + str(self.object_id) + ' [' + str(self.date) + ']'
+
+    class Meta:
+        verbose_name_plural = u'Journal Entries'
+
+
+class Transaction(models.Model):
+    account = models.ForeignKey(Account)
+    dr_amount = models.FloatField(null=True, blank=True)
+    cr_amount = models.FloatField(null=True, blank=True)
+    current_dr = models.FloatField(null=True, blank=True)
+    current_cr = models.FloatField(null=True, blank=True)
+    journal_entry = models.ForeignKey(JournalEntry, related_name='transactions')
+
+    def get_balance(self):
+        return zero_for_none(self.current_dr) - zero_for_none(self.current_cr)
+
+    def __str__(self):
+        return str(self.account) + ' [' + str(self.dr_amount) + ' / ' + str(self.cr_amount) + ']'
+
+
+def alter(account, date, dr_difference, cr_difference):
+    Transaction.objects.filter(journal_entry__date__gt=date, account=account).update(
+        current_dr=none_for_zero(zero_for_none(F('current_dr')) + zero_for_none(dr_difference)),
+        current_cr=none_for_zero(zero_for_none(F('current_cr')) + zero_for_none(cr_difference)))
+
+
+def set_transactions(submodel, date, *args):
+    if isinstance(date, unicode):
+        date = datetime.datetime.strptime(date, '%Y-%m-%d')
+    journal_entry, created = JournalEntry.objects.get_or_create(
+        content_type=ContentType.objects.get_for_model(submodel), object_id=submodel.id,
+        defaults={
+            'date': date
+        })
+    for arg in args:
+        # transaction = Transaction(account=arg[1], dr_amount=arg[2])
+        matches = journal_entry.transactions.filter(account=arg[1])
+        if not matches:
+            transaction = Transaction()
+            transaction.account = arg[1]
+            if arg[0] == 'dr':
+                transaction.dr_amount = float(zero_for_none(arg[2]))
+                transaction.cr_amount = None
+                transaction.account.current_dr = none_for_zero(
+                    zero_for_none(transaction.account.current_dr) + transaction.dr_amount)
+                alter(arg[1], date, float(arg[2]), 0)
+            if arg[0] == 'cr':
+                transaction.cr_amount = float(zero_for_none(arg[2]))
+                transaction.dr_amount = None
+                transaction.account.current_cr = none_for_zero(
+                    zero_for_none(transaction.account.current_cr) + transaction.cr_amount)
+                alter(arg[1], date, 0, float(arg[2]))
+            transaction.current_dr = none_for_zero(
+                zero_for_none(transaction.account.get_dr_amount(date + datetime.timedelta(days=1)))
+                + zero_for_none(transaction.dr_amount))
+            transaction.current_cr = none_for_zero(
+                zero_for_none(transaction.account.get_cr_amount(date + datetime.timedelta(days=1)))
+                + zero_for_none(transaction.cr_amount))
+        else:
+            transaction = matches[0]
+            transaction.account = arg[1]
+
+            # cancel out existing dr_amount and cr_amount from current_dr and current_cr
+            # if transaction.dr_amount:
+            #     transaction.current_dr -= transaction.dr_amount
+            #     transaction.account.current_dr -= transaction.dr_amount
+            #
+            # if transaction.cr_amount:
+            #     transaction.current_cr -= transaction.cr_amount
+            #     transaction.account.current_cr -= transaction.cr_amount
+
+            # save new dr_amount and add it to current_dr/cr
+            if arg[0] == 'dr':
+                dr_difference = float(arg[2]) - zero_for_none(transaction.dr_amount)
+                cr_difference = zero_for_none(transaction.cr_amount) * -1
+                alter(arg[1], transaction.journal_entry.date, dr_difference, cr_difference)
+                transaction.dr_amount = float(arg[2])
+                transaction.cr_amount = None
+            else:
+                cr_difference = float(arg[2]) - zero_for_none(transaction.cr_amount)
+                dr_difference = zero_for_none(transaction.dr_amount) * -1
+                alter(arg[1], transaction.journal_entry.date, dr_difference, cr_difference)
+                transaction.cr_amount = float(arg[2])
+                transaction.dr_amount = None
+
+            transaction.current_dr = none_for_zero(zero_for_none(transaction.current_dr) + dr_difference)
+            transaction.current_cr = none_for_zero(zero_for_none(transaction.current_cr) + cr_difference)
+            transaction.account.current_dr = none_for_zero(
+                zero_for_none(transaction.account.current_dr) + dr_difference)
+            transaction.account.current_cr = none_for_zero(
+                zero_for_none(transaction.account.current_cr) + cr_difference)
+
+        # the following code lies outside if,else block, inside for loop
+        transaction.account.save()
+        try:
+            journal_entry.transactions.add(transaction, bulk=False)
+        except TypeError:  # for Django <1.9
+            journal_entry.transactions.add(transaction)
+
+
+def delete_rows(rows, model):
+    for row in rows:
+        if row.get('id'):
+            instance = model.objects.get(id=row.get('id'))
+            try:
+                JournalEntry.objects.get(content_type=ContentType.objects.get_for_model(model),
+                                         model_id=instance.id).delete()
+            except:
+                pass
+            instance.delete()
+
+
+@receiver(pre_delete, sender=Transaction)
+def _transaction_delete(sender, instance, **kwargs):
+    transaction = instance
+    # cancel out existing dr_amount and cr_amount from account's current_dr and current_cr
+    if transaction.dr_amount:
+        transaction.account.current_dr -= transaction.dr_amount
+
+    if transaction.cr_amount:
+        transaction.account.current_cr -= transaction.cr_amount
+
+    alter(transaction.account, transaction.journal_entry.date, float(zero_for_none(transaction.dr_amount)) * -1,
+          float(zero_for_none(transaction.cr_amount)) * -1)
+
+    transaction.account.save()
