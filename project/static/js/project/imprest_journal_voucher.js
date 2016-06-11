c0264038170013f8e2760fd1aa43415f208dca2b
Dipesh Acharya <xtranophilist@gmail.com>
2016-06-11 13:59:47 +0545

    Fix Imprest JV loading

--- project/static/js/project/imprest_journal_voucher.js
+++ project/static/js/project/imprest_journal_voucher.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
 
 function ImprestJV(data) {
     var self = this;
-    console.log(data);
 
+    self.id = ko.observable();
     self.voucher_no = ko.observable();
     self.date = ko.observable();
     self.dr = ko.observable();
@@ -16,30 +16,35 @@ function ImprestJV(data) {
     self.amount_nrs = ko.observable();
     self.amount_usd = ko.observable();
     self.exchange_rate = ko.observable();
-    
+
     self.dr_ledger = ko.observable();
     self.cr_ledger = ko.observable();
 
     self.dr_ledgers = ko.observableArray();
     self.cr_ledgers = ko.observableArray();
 
+    for (var k in data.jv){
+        if (ko.isObservable(self[k]))
+            self[k](data.jv[k]);
+    }
+
     ko.utils.arrayForEach(data.dr_ledgers, function (obj) {
         self.dr_ledgers.push(obj);
     });
-    
+
     ko.utils.arrayForEach(data.cr_ledgers, function (obj) {
         self.cr_ledgers.push(obj);
     });
 
 
     self.status = ko.observable('Loading...');
-    
-    self.save = function(){
+
+    self.save = function () {
         if (!self.date()) {
             alert.error('Date is required!');
             return false;
         }
         return true;
     }
-    
+
 }
