32a56543f00b0da1014b8fda2bd31eef752aa807
Dipesh Acharya <xtranophilist@gmail.com>
2016-07-03 14:23:46 +0545

    Fix conversion link for bs date

---
+++ account/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('account', '0003_merge'),
+        ('account', '0004_auto_20160702_1422'),
+    ]
+
+    operations = [
+    ]
--- users/templatetags/filters.py
+++ users/templatetags/filters.py
@@ -12,6 +12,7 @@ from django.db.models import Model
 from django import template
 from django.contrib.auth.models import Group
 from django.utils.translation import get_language
+from njango.nepdate import bs2ad
 
 from njango.utils import get_calendar
 
@@ -58,6 +59,14 @@ def handler(obj):
         return []
     else:
         raise TypeError, 'Object of type %s with value of %s is not JSON serializable' % (type(obj), repr(obj))
+
+
+@register.filter
+def Ymd(dt):
+    if type(dt) == date:
+        return str(dt)
+    else:
+        return str(date(*bs2ad(dt)))
 
 
 @register.filter
