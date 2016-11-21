7df4127d6e082703ce6967e18e94229e20ef2b75
Rupesh Singh <wrufesh@awecode.com>
2016-11-21 21:06:08 +0545

    employee filter minor fixes

--- hr/filters.py
+++ hr/filters.py
@@ -13,22 +13,18 @@ class EmployeeFilter(django_filters.FilterSet):
         accountant_branch_id = kwargs.pop('accountant_branch_id')
         super(EmployeeFilter, self).__init__(*args, **kwargs)
 
-
-
         if PayrollConfig.get_solo().parent_can_generate_payroll:
             self.form.fields['working_branch'].queryset = BranchOffice.objects.get(
                     id=accountant_branch_id).get_descendants(include_self=True)
-
         else:
-            self.form.fields['working_branch'] = BranchOffice.objects.filter(id=accountant_branch_id)
-
-
+            self.form.fields['working_branch'].queryset = BranchOffice.objects.filter(id=accountant_branch_id)
+        self.form.fields['working_branch'].empty_label = None
 
     is_active = django_filters.BooleanFilter(help_text='')
 
     working_branch = TreeNodeModelChoiceFilter(
         queryset=BranchOffice.objects.all(),
-        help_text=''
+        help_text='',
     )
 
     class Meta:
